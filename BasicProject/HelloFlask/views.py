358f0a69ec236de6cc3e5dafe27103fa40234d6c
Tyrone Hills <45140197+yididiel7@users.noreply.github.com>
2020-04-19 21:45:55 +0300

    updated the home function to provide values for all the placeholders:

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,15 @@
+from datetime import datetime
 from flask import Flask
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    html_content = "<html><head><title>Hello Flask</title></head><body>"
+    html_content += "<strong>Hello Flask!</strong> on " + formatted_now
+    html_content += "</body></html>"
+
+    return html_content
